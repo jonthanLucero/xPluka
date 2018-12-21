1f0c81b5a6d5598805649ea8e28560fec708074b
Jonathan Lucero <jlucero@inalambrik.com.ec>
2018-12-21 09:19:22 -0500

    Final Fixes and details

---
+++ xPluka/CustomViewCell/VisitTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  VisitCell.swift
+//  xPluka
+//
+//  Created by Peter Arcentales on 12/18/18.
+//  Copyright © 2018 Inalambrilk. All rights reserved.
+//
+
+import UIKit
+
+class VisitTableViewCell: UITableViewCell {
+    
+    static let reuseIdentifier = "VisitCell"
+    
+    //It declares the outlet controls used in the viewcell
+    @IBOutlet weak var touristicPlaceImageType: UIImageView!
+    @IBOutlet weak var touristicPlaceName: UILabel!
+    @IBOutlet weak var startDate: UILabel!
+    @IBOutlet weak var endYear: UILabel!
+    @IBOutlet weak var startYear: UILabel!
+    @IBOutlet weak var endDate: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
